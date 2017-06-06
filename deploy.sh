ce555600c77b74096ff6d05c20eac444ff23f1de
Tom Richardson <tgrkzus@gmail.com>
2017-06-06 19:31:27 +1000

    basic setup with deploy script

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+function deploy
+{
+	aws s3 cp . s3://tomgr.io/ --recursive --exclude "*" --include "*.html" --include "*.js" --include "*.css" --acl public-read
+}
+
+function clean
+{
+	aws s3 rm --recursive s3://tomgr.io/
+}
+
+if [ "$1" == "clean" ]; then
+	clean
+else
+	deploy
+fi
